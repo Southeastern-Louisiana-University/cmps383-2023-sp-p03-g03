3337559c0f44be654f4841069e273db72b879a5f
Chase <chase.leimbach@selu.edu>
2023-03-12 18:12:12 -0500

    Frontend not showing fixed

--- SP23.P03.Web/ClientApp/src/App.tsx
+++ SP23.P03.Web/ClientApp/src/App.tsx
@@ -1,30 +1,21 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import { PageRoutes } from './pageroute/pageroutes';
 import { HomePage } from './pages/Home/home-page';
 import { NotFoundPage } from './pages/NotFound/notfound-page';
+import { routes } from './Routes/config';
+export function App() : React.ReactElement {
 
-export const routes = {
-  home : '/',
-}
-
-function App() {
   return (
-  <div>
-      <div>Hello</div>
-  </div>
- 
+    <div>
+        <Routes>
+          <Route path={routes.home} element={<HomePage />} />
+          {/* If the path does not exist */}
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+    </div>
+
   );
+
 }
-
 export default App;
-
-// <Routes>
-// <Route path={PageRoutes.HOME} element={<HomePage />} />
-// {/* Page Not Found */}
-// <Route path="*" element={<NotFoundPage />} />
-// </Routes>
-
-//*** ^^^ the 5 lines of code should be within the first div before the div with hello,
-//*** just nothing renders on the page when trying to route to another.
