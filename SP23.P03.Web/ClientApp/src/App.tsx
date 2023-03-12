f3d0fe8da3c1479e7cca17fb1c7c14e0605b29ab
Dawso125 <dawson.davis@selu.edu>
2023-03-12 18:37:35 -0500

    ROUTING EXAMPLES
    Please refer to the homePage to see how to route between your pages. App.tsx contains your routes now, routes are also stored in the routes config.tsx

--- SP23.P03.Web/ClientApp/src/App.tsx
+++ SP23.P03.Web/ClientApp/src/App.tsx
@@ -3,7 +3,11 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { HomePage } from './pages/Home/home-page';
 import { NotFoundPage } from './pages/NotFound/notfound-page';
+import { SignUpPage } from './pages/SignUp-Page/signUp-Page';
 import { routes } from './Routes/config';
+
+
+
 export function App() : React.ReactElement {
 
   return (
@@ -12,6 +16,7 @@ export function App() : React.ReactElement {
           <Route path={routes.home} element={<HomePage />} />
           {/* If the path does not exist */}
           <Route path="*" element={<NotFoundPage />} />
+          <Route path={routes.signup} element={<SignUpPage/>}/>
         </Routes>
     </div>
 
