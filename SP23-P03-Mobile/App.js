de08291f52a4c7e160bc8747f43d3a301e1f6d74
Quincy Williams <quincy.williams-2@selu.edu>
2023-03-12 15:12:30 -0600

    Changes for App

--- SP23-P03-Mobile/App.js
+++ SP23-P03-Mobile/App.js
@@ -1,4 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
+import {NavigationContainer} from "@react-navigation/native";
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
@@ -6,6 +7,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      
     </View>
   );
 }
