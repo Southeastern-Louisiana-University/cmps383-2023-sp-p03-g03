2e07083702438a916d3516d2f23bbe4cfcab03f0
Chase <chase.leimbach@selu.edu>
2023-03-13 13:01:42 -0500

    Trying to get Home Page to render CSS properly
    - Wont recognize the css file (claims module not found)

--- SP23.P03.Web/ClientApp/src/pages/Home/home-page.tsx
+++ SP23.P03.Web/ClientApp/src/pages/Home/home-page.tsx
@@ -1,19 +1,52 @@
 import React from 'react';
 import { useNavigate} from 'react-router-dom';
 import { routes } from '../../Routes/config';
+//import './Home/home-page.css';
 
 
 export function HomePage(): React.ReactElement {
     const navigate = useNavigate();
     return (
-        <div>
-            <div>Home Page</div>
-            <button onClick={() => {navigate('*')}}>
-                not found
-            </button>
-            <button onClick={() => {navigate(routes.signup)}}>
-                SignUp Page
-            </button>
-        </div>
+        
+<body>
+  <nav>
+    <div className="menu">
+      <div className="logo">
+        <a href="#">Entrack</a>
+      </div>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Contact</a></li>
+        <li><a href="#">Feedback</a></li>
+      </ul>
+    </div>
+  </nav>
+  <div className="img"></div>
+  <div className="center">
+    <div className="title">Come Coasting Today!</div>
+    <div className="sub_title">Trips For Everyone</div>
+    <div className="btns">
+      <button>Sign-In</button>
+      <button onClick={() => {navigate(routes.signup)}}>Sign-Up</button>
+    </div>
+  </div>
+</body>
+
     );
 }
+
+
+
+
+
+/// <div>
+/// <div>Home Page</div>
+/// <button onClick={() => {navigate('*')}}>
+///     not found
+/// </button>
+/// <button onClick={() => {navigate(routes.signup)}}>
+///     SignUp Page
+/// </button>
+/// </div>
