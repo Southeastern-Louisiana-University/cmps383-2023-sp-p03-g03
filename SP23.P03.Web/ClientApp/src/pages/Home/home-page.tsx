f5c2758fc5f95891c15e406a4c91c40413ab3fb2
Dawso125 <dawson.davis@selu.edu>
2023-03-13 14:07:59 -0500

    fixed home page css

--- SP23.P03.Web/ClientApp/src/pages/Home/home-page.tsx
+++ SP23.P03.Web/ClientApp/src/pages/Home/home-page.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate} from 'react-router-dom';
 import { routes } from '../../Routes/config';
-//import './Home/home-page.css';
+import '../Home/home-page.css';
 
 
 export function HomePage(): React.ReactElement {
