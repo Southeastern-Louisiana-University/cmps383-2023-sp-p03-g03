1ee20952ecc58cd920d4e9182c78aec777566e7b
Dawso125 <dawson.davis@selu.edu>
2023-03-13 14:15:33 -0500

    fixed all the css related warnings as well.

--- SP23.P03.Web/ClientApp/src/pages/Home/home-page.tsx
+++ SP23.P03.Web/ClientApp/src/pages/Home/home-page.tsx
@@ -12,14 +12,14 @@ export function HomePage(): React.ReactElement {
   <nav>
     <div className="menu">
       <div className="logo">
-        <a href="#">Entrack</a>
+        <a href="/#">Entrack</a>
       </div>
       <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Services</a></li>
-        <li><a href="#">Contact</a></li>
-        <li><a href="#">Feedback</a></li>
+        <li><a href="/#">Home</a></li>
+        <li><a href="/#">About</a></li>
+        <li><a href="/#">Services</a></li>
+        <li><a href="/#">Contact</a></li>
+        <li><a href="/#">Feedback</a></li>
       </ul>
     </div>
   </nav>
