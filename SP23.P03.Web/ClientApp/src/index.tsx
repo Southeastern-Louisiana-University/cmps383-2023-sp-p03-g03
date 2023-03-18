18177c5fbde64595dacca4c23b5399731e2e2cc2
Chase <chase.leimbach@selu.edu>
2023-03-18 18:57:20 -0500

    Semi Working Auth
    - Still having issues with AuthProvider

--- SP23.P03.Web/ClientApp/src/index.tsx
+++ SP23.P03.Web/ClientApp/src/index.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './components/AuthProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+    <AuthProvider>
       <App />
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
