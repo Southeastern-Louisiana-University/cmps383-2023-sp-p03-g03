313c32c66295a7eb942dbd45453c61f667ce2efc
Quincy Williams <quincy.williams-2@selu.edu>
2023-03-29 21:22:36 -0500

    Ticketsdto added

--- SP23.P03.Web/Features/Tickets/TicketsDto.cs
+++ SP23.P03.Web/Features/Tickets/TicketsDto.cs
@@ -1,6 +1,12 @@
-﻿namespace SP23.P03.Web.Features.Tickets
+﻿using SP23.P03.Web.Features.Trips;
+
+namespace SP23.P03.Web.Features.Tickets
 {
     public class TicketsDto
     {
+        public int Id { get; set; }
+        public float Price { get; set; }
+        public int TripId { get; set; }
+        public Trip Trip { get; set; }
     }
 }
