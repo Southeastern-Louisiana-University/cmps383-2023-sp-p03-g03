7e983f745f48a32876dd102dbb919e550d1a088f
Dawso125 <dawson.davis@selu.edu>
2023-03-31 16:50:03 -0500

    fixes

--- SP23.P03.Web/Features/Tickets/TicketsDto.cs
+++ SP23.P03.Web/Features/Tickets/TicketsDto.cs
@@ -1,11 +1,17 @@
 ﻿namespace SP23.P03.Web.Features.Tickets
 {
+    public enum TicketTypesDto
+    {
+        Coach,
+        Luxury,
+        FirstClass
+    }
     public class TicketsDto
     {
         public int Id { get; set; }
         public float Price { get; set; }
         public int RouteId { get; set; }
-        public TicketTypes TicketType { get; set; }
+        public TicketTypesDto TicketType { get; set; }
        
     }
 }
