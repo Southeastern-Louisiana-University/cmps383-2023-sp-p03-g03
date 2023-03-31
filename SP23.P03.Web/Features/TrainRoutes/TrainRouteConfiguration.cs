a7790a0a912ef5abb657cf3fe92d419fdbf9b3f6
Dawso125 <dawson.davis@selu.edu>
2023-03-31 16:47:22 -0500

    TrainRoute Config DONE

---
+++ SP23.P03.Web/Features/TrainRoutes/TrainRouteConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace SP23.P03.Web.Features.TrainRoutes
+{
+    public class TrainRouteConfiguration : IEntityTypeConfiguration<TrainRoute>
+    {
+        public void Configure(EntityTypeBuilder<TrainRoute> builder)
+        {
+            builder.Property(x => x.TripDate)
+                .IsRequired();
+
+            builder.Property(x => x.StartingDestinationId)
+                .IsRequired();
+
+            builder.Property(x => x.EndingDestinationId)
+               .IsRequired();
+        }
+    }
+}
