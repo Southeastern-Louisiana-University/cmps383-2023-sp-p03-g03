0aa6b0ff580f5a066c5f076443cd6be34ba11463
Dawso125 <dawson.davis@selu.edu>
2023-03-31 16:32:50 -0500

    Added TrainRoute Dto

---
+++ SP23.P03.Web/Features/TrainRoutes/TrainRouteDto.cs
@@ -0,0 +1,14 @@
+﻿using SP23.P03.Web.Features.Destinations;
+
+namespace SP23.P03.Web.Features.TrainRoutes
+{
+    public class TrainRouteDto
+    {
+        public int Id { get; set; }
+        public DateTimeOffset TripDate { get; set; }
+        public int StartingDestinationId { get; set; }
+        public int EndingDestinationId { get; set; }
+        public Destination StartingDestination { get; set; }
+        public Destination EndingDestination { get; set; }
+    }
+}
