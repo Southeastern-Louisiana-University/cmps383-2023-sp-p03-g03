cb39ca7b4fa6886d106478be39ce52f3158c504e
Dawso125 <dawson.davis@selu.edu>
2023-04-02 21:07:04 -0500

    Added Post

--- SP23.P03.Web/Controllers/TrainRoutesController.cs
+++ SP23.P03.Web/Controllers/TrainRoutesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
 using SP23.P03.Web.Data;
 using SP23.P03.Web.Features;
 using SP23.P03.Web.Features.TrainRoutes;
@@ -40,6 +41,27 @@ namespace SP23.P03.Web.Controllers
             return Ok(result);
         }
 
+        [HttpPost]
+        public ActionResult<TrainRouteDto> CreateTrainRoute(TrainRouteDto dto)
+        {
+            if (IsInvalid(dto))
+            {
+                return BadRequest();
+            }
+
+            var trainRoute = new TrainRoute
+            {
+                TripDate = dto.TripDate,
+                TripType = (TripTypes)dto.TripType,
+                StartingDestinationId = dto.StartingDestinationId,
+                EndingDestinationId = dto.EndingDestinationId,
+            };
+            trainRoutes.Add(trainRoute);
+            dataContext.SaveChanges();
+            dto.Id = trainRoute.Id;
+            return CreatedAtAction(nameof(GetTrainRouteById), new {id = dto.Id}, dto);
+        }
+
         private IQueryable<TrainRouteDto> GetTrainRouteDtos(IQueryable<TrainRoute> trainRoutes)
         {
             return trainRoutes
@@ -51,6 +73,13 @@ namespace SP23.P03.Web.Controllers
                     StartingDestinationId = x.StartingDestinationId,
                     EndingDestinationId = x.EndingDestinationId,
                 });
+        }
+        private bool IsInvalid(TrainRouteDto dto)
+        {
+            return int.IsNegative(dto.StartingDestinationId) ||
+                   dto.StartingDestinationId == 0 ||
+                   int.IsNegative(dto.EndingDestinationId) ||
+                   dto.EndingDestinationId == 0;
         }
     }
 }
