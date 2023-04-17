61723c74997ad5bf22fce92609ff42a587a251cb
Quincy Williams <quincy.williams-2@selu.edu>
2023-04-17 17:48:43 -0500

    App-HomePage
    Simple start to the homepage of the app.

--- SP23-P03-Mobile/App.js
+++ SP23-P03-Mobile/App.js
@@ -1,22 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
 import {NavigationContainer} from "@react-navigation/native";
-import { StyleSheet, Text, View } from 'react-native';
+import React, {userState, useEffect} from 'react' ;
+import { SafeAreaView, 
+  StyleSheet, 
+  Text, 
+  View, 
+   } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      
-    </View>
-  );
-}
+   const HomePage = () => {
+    const handleStartTrip = () => {
+      console.log('Starting trip today');
+      // Navigate to the trip creation or selection screen
+    };
+  
+    return (
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.title}>Welcome to Trip App</Text>
+        <TouchableOpacity style={styles.button} onPress={handleStartTrip}>
+          <Text style={styles.buttonText}>Start a Trip Today</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+    );
+  };
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      paddingHorizontal: 20,
+    },
+    title: {
+      fontSize: 24,
+      fontWeight: 'bold',
+      marginBottom: 20,
+    },
+    button: {
+      backgroundColor: '#007bff',
+      borderRadius: 5,
+      padding: 10,
+      paddingHorizontal: 20,
+    },
+    buttonText: {
+      color: '#fff',
+      fontSize: 18,
+      fontWeight: '600',
+    },
+  });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  export default HomePage;
