97fa65d168aa428e12283dd54f56334d2bb5610c
Quincy Williams <quincy.williams-2@selu.edu>
2023-04-22 23:10:44 -0500

    Pushing final app setup
    Destination page

--- SP23-P03-Mobile/App.js
+++ SP23-P03-Mobile/App.js
@@ -1,150 +1,120 @@
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import React, { useState } from 'react';
+
 import { Header, Icon } from 'react-native-elements';
-import {StatusBar } from 'expo-status-bar';
-import {NavigationContainer} from "@react-navigation/native";
-import { SafeAreaView, 
-  StyleSheet, 
-  Text, 
-  TouchableOpacity, 
+
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
   ImageBackground,
-  View, 
-  Image,} from 'react-native';
+  View,
+  Image,
+  ScrollView, // Add this line
+} from 'react-native';
 
-   const HomePage = () => {
-    const handleSignIn = () => {
-      console.log('Sign In');
-      //Navigation to signin screen
-    };
+const HomePage = () => {
+  const [selectedRoute, setSelectedRoute] = useState(null);
 
-    const handleSignUp = () => {
-      console.log('Sign Up');
-      //Navigate to the signup page
-    };
+  const handleRouteSelection = (route) => {
+    setSelectedRoute(route);
+  };
 
-    
+  const trainRoutes = [
+    { id: 1, name: 'Route 1', price: '$300' },
+    { id: 2, name: 'Route 2', price: '$400' },
+    { id: 3, name: 'Route 3', price: '$500' },
+    // Add more train routes if needed
+  ];
 
-    const toggleDrawer = () => {
-      navigation.toggleDrawer();
-    };
-    
-  
-    return (
-      
-      
-      <SafeAreaView style={styles.container}>
-        <Header
-        rightComponent={<Icon 
-          name="menu"
-          type="material"
-          color="#fff"
-          onPress={toggleDrawer}/>}
+  return (
+    <SafeAreaView style={styles.container}>
+      <Header
+        rightComponent={
+          <Icon name="menu" type="material" color="#fff" onPress={() => {}} /> // Add drawer functionality if needed
+        }
         leftComponent={{ text: 'Entrack', style: { color: '#fff', fontSize: 18 } }}
         backgroundColor="#000"
       />
-      
-        
-        <ImageBackground source={{ uri: 'https://unsplash.com/photos/oEIFOoC3gi0/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8Mnx8dHJhaW5zJTIwaW4lMjBtb3VudGFpbnN8ZW58MHx8fHwxNjgxODc4NTM5&force=true&w=2400' }} style={styles.imageBackground} resizeMode="cover">
-          <Image source={{ uri: 'https://your-logo-url.com/path/to/logo.png' }} style={styles.logo} /> 
-          <Text style={styles.title}>Welcome to Entrack</Text>
-          <View style={styles.buttonsContainer}>
-          <TouchableOpacity style={styles.button} onPress={handleSignIn}>
-            <Text style={styles.buttonText}>Sign In</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={handleSignUp}>
-            <Text style={styles.buttonText}>Sign Up</Text>
+      <ImageBackground
+        source={{ uri: 'https://unsplash.com/photos/oEIFOoC3gi0/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8Mnx8dHJhaW5zJTIwaW4lMjBtb3VudGFpbnN8ZW58MHx8fHwxNjgxODc4NTM5&force=true&w=2400' }}
+        style={styles.imageBackground}
+        resizeMode="cover"
+      >
+        <Text style={styles.title}>Choose a Train Route</Text>
+        <ScrollView style={styles.routesContainer}>
+          {trainRoutes.map((route) => (
+            <TouchableOpacity key={route.id} style={styles.routeCard} onPress={() => handleRouteSelection(route)}>
+              <Text style={styles.routeName}>{route.name}</Text>
+              <Text style={styles.routePrice}>{route.price}</Text>
+              {selectedRoute && selectedRoute.id === route.id && (
+                <QRCode value={route.id.toString()} size={80} />
+              )}
             </TouchableOpacity>
-            </View>
-         </ImageBackground>
-         <View style={{
-              backgroundColor: '#000',
-              height: 70,
-              width: '100%',
-              justifyContent: 'center',
-              alignItems: 'center',
-               }}
-              ><Text style={{ color: '#fff', fontSize: 12 }}>{'\u00A9'} {(new Date()).getFullYear()} Entrack. All rights reserved.</Text> 
-              </View>
-        
-          </SafeAreaView>
-      
-    );
-  };
-  const Drawer = createDrawerNavigator();
-
-  const App = () => {
-    return (
-      <NavigationContainer>
-        <Drawer.Navigator initialRouteName="Home">
-          < Drawer.Screen name="Home" component={HomePage} />
-          {/* Add more screens here if needed */}
-        </Drawer.Navigator>
-      </NavigationContainer>
-    );
-  };
+          ))}
+        </ScrollView>
+        {selectedRoute && (
+          <TouchableOpacity style={styles.buyButton}>
+            <Text style={styles.buttonText}>Buy Ticket</Text>
+          </TouchableOpacity>
+        )}
+      </ImageBackground>
+    </SafeAreaView>
+  );
+};
+  
 
 
 
-
-  const styles = StyleSheet.create({
-    safeArea: {
-      flex: 1,
-    },
-    topBar: {
-      backgroundColor: '#000',
-      height: 70,
-      width: '100%',
-    },
-    bottomBar: {
-      backgroundColor: '#000',
-      height: 50,
-      width: '100%',
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    copyrightText: {
-      color: '#FFF',
-      fontSize: 12,
-    },
-    container: {
-      flex: 1,
-    },
-      imageBackground: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        paddingHorizontal: 20,
-      },
-      logo: {
-        width: 100,
-        heigth: 100,
-        marginBottom: 20,
-      },
-    title: {
-      fontSize: 24,
-      fontWeight: 'bold',
-      marginBottom: 20,
-      color: '#FFF',
-    },
-    buttonsContainer: {
-      flexDirection: 'row',
-      flexWrap: 'wrap',
-    },
-
-    button: {
-      backgroundColor: '#007bff',
-      borderRadius: 5,
-      padding: 10,
-      paddingHorizontal: 20,
-    },
-    signInButton: {
-      marginRight: 10,
-    },
-
-    buttonText: {
-      color: '#fff',
-      fontSize: 18,
-      fontWeight: '600',
-    },
-  });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  imageBackground: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: '#FFF',
+  },
+  routesContainer: {
+    width: '100%',
+  },
+  routeCard: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 5,
+  },
+  routeName: {
+    fontSize: 18,
+    marginRight: 10,
+  },
+  routePrice: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginRight: 10,
+  },
+  buyButton: {
+    backgroundColor: '#007bff',
+    borderRadius: 5,
+    padding: 10,
+    paddingHorizontal: 20,
+    marginTop: 20,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: '600',
+  },
+});    
 
   export default HomePage;
